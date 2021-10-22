815c809487faed1aaa2f196cde354f0ff0ada898
mi-touch <timileyinadefare@yahoo.com>
2021-10-22 07:36:00 +0100

    This adjust the pixel size of each image

---
+++ adjust.m
@@ -0,0 +1,8 @@
+img = imread('8.png');
+pout = rgb2gray(img);
+pout_imadjust = imadjust(pout);
+pout_histeq = histeq(pout);
+pout_adapthisteq = adapthisteq(pout);
+
+montage({pout,pout_imadjust,pout_histeq,pout_adapthisteq},'Size',[1 4])
+title("Original Image and Enhanced Images using imadjust, histeq, and adapthisteq")
